dab8d92a999f7fafbeebd654e76ae87236b1a829
Zaw <zawlinnnaing0018@gmail.com>
2023-12-19 00:04:54 +0700

    day12: finished part1

--- day12/part2.go
+++ day12/part2.go
@@ -3,13 +3,9 @@ package main
 import "fmt"
 
 func Unfold(input Input) Input {
-	//originalParts := []string{}
-	//copy(input.parts, originalParts)
-	//originalRecords := []int{}
-	//copy(input.records, originalRecords)
 	partsLen := len(input.parts)
 	recordsLen := len(input.records)
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 4; i++ {
 		input.parts = append(input.parts, append([]string{UNKNOWN}, input.parts[:partsLen]...)...)
 		input.records = append(input.records, input.records[:recordsLen]...)
 	}
@@ -20,7 +16,7 @@ func RunPart2(inputs []Input) int {
 	total := 0
 	for _, input := range inputs {
 		unfolded := Unfold(input)
-		cacheMap := map[string]map[string]int{}
+		//cacheMap := map[string]map[string]int{}
 		result := count(unfolded.parts, unfolded.records)
 		fmt.Println(unfolded, result)
 		total += result
